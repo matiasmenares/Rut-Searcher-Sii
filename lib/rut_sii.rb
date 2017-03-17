52a457ee5e93d7edb7e33caa1bade0794aa61147
matiasmenares <dejaaymts@gmail.com>
2017-03-16 23:16:30 -0300

    to son

--- lib/rut_sii.rb
+++ lib/rut_sii.rb
@@ -1,11 +1,12 @@
 module RutSii
-	require 'net/http'
+ 	require 'net/http'
+ 	require 'json'
 	def self.query value
 		uri = URI.parse("http://api-sii.apptec.cl/api/v1/companies")
 		params = { :rut => value }
 		uri.query = URI.encode_www_form(params)
 
 		res = Net::HTTP.get_response(uri)
-		return res.body
+		return JSON.parse(res.body)
 	end
 end
